d9cf176d67403d32319c52362160bf84749d428c
Chloe Rees <reeschloe@yahoo.com>
2022-07-28 10:30:43 -0500

    added create and update ability for observations

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import { listObservations } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
+import { useHistory } from "react-router-dom"
+
 function Home() {
+  const history = useHistory();
+  const [observations, setObservations] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    listObservations(abortController.signal)
+      .then(setObservations)
+      .catch(setError);
+    return () => abortController.abort();
+  }, []);
+
+  const tableRows = observations.map((observation) => (
+    <tr key={observation.observation_id}>
+      <th scope="row">{observation.observation_id}</th>
+      <td>{observation.latitude}</td>
+      <td>{observation.longitude}</td>
+      <td>{observation.sky_condition}</td>
+      <td>{observation.created_at}</td>
+      <td><button onClick={() => history.push(`/observations/${observation.observation_id}/edit`)}>Edit</button></td>
+    </tr>
+  ));
+
   return (
     <main>
       <h1>Home</h1>
+      <ErrorAlert error={error} />
+      <table className="table">
+        <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Latitude</th>
+          <th scope="col">Longitude</th>
+          <th scope="col">Sky Condition</th>
+          <th scope="col">Created</th>
+          <th scope="col">Edit</th>
+        </tr>
+        </thead>
+        <tbody>
+        {tableRows}
+        </tbody>
+      </table>
     </main>
   );
 }
